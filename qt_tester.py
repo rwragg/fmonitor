3a605490fd540ced5a7d0a1ce883fd7fd48bb095
Robert J Wragg <robert.wragg@gmail.com>
2020-03-13 09:57:58 -0600

    updated qt_test.py

--- qt_tester.py
+++ qt_tester.py
@@ -1,9 +1,8 @@
 # Simple enough, just import everything from tkinter.
 from tkinter import *
+from ui import *
 
 
-# Here, we are creating our class, Window, and inheriting from the Frame
-# class. Frame is a class from the tkinter module. (see Lib/tkinter/__init__)
 class Window(Frame):
 
     # Define settings upon initialization. Here you can specify
@@ -20,48 +19,24 @@ class Window(Frame):
     # Creation of init_window
     def init_window(self):
         # changing the title of our master widget
-        self.master.title("GUI")
+        self.master.title("Visual Grep")
         self.master.option_add('*tearOff', False)
 
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
 
         # creating a menu instance
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
-
-        # create the file object)
-        file = Menu(menu)
-
-        # adds a command to the menu option, calling it exit, and the
-        # command it runs on event is client_exit
-        file.add_command(label="Exit", command=self.client_exit)
-
-        # added "file" to our menu
-        menu.add_cascade(label="File", menu=file)
-
-        # create the file object)
-        edit = Menu(menu)
-
-        # adds a command to the menu option, calling it exit, and the
-        # command it runs on event is client_exit
-        edit.add_command(label="Undo")
-
-        # added "file" to our menu
-        menu.add_cascade(label="Edit", menu=edit)
-
-    def client_exit(self):
-        exit()
+        menubar = MenuBar(self.master)
 
 
 # root window created. Here, that would be the only window, but
 # you can later have windows within windows.
-root = Tk()
+if __name__ == "__main__":
+    root = Tk()
+    root.geometry("800x600")
 
-root.geometry("400x300")
+    # creation of an instance
+    app = Window(root)
 
-# creation of an instance
-app = Window(root)
-
-# mainloop
-root.mainloop()  
+    # mainloop
+    root.mainloop()
---
+++ ui/MenuBar.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+
+class MenuBar(object):
+    def __init__(self, master):
+        self.master = master
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        # create the file object)
+        file = Menu(menu)
+
+        # adds a command to the menu option, calling it exit, and the
+        # command it runs on event is client_exit
+        file.add_command(label="Exit", command=self.client_exit)
+
+        # added "file" to our menu
+        menu.add_cascade(label="File", menu=file)
+
+        # create the file object)
+        edit = Menu(menu)
+
+        # adds a command to the menu option, calling it exit, and the
+        # command it runs on event is client_exit
+        edit.add_command(label="Undo")
+
+        # added "file" to our menu
+        menu.add_cascade(label="Edit", menu=edit)
+
+    def client_exit(self):
+        exit()
